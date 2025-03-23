7c66ed863ee79d8be180c1497c4c267c9d2e4ee5
Adrian Caderno <adriancaderno@gmail.com>
2025-03-23 20:14:18 +0100

    Added posibility to configure thresholds of confidence and IoU

--- yolo_tracking.py
+++ yolo_tracking.py
@@ -21,4 +21,7 @@ root = tk.Tk()
 root.withdraw()  # Hide the root window
 video_path = filedialog.askopenfilename(title="Select the video file", filetypes=[("Video files", "*.mp4;*.avi;*.mov")])
 
-results = model.track(video_path, show=True)  # Tracking with default tracker
+#results = model.track(video_path, show=True)  # Tracking with default tracker
+results = model.track(video_path, conf=0.3, iou=0.5, show=True)  # Tracking with confidence and IoU threshold
+#results = model.track(video_path, show=True, tracker='tracker_config/bytetrack.yaml')  # with ByteTrack
+#results = model.track(video_path, show=True, tracker='tracker_config/botsort.yaml')  # with BoT-SORT
