e503c30882d178c91fc2450745c52e6f53bd4d92
Adrian Caderno <adriancaderno@gmail.com>
2025-03-23 19:45:36 +0100

    Added code to open video file from dialog window

--- yolo_tracking.py
+++ yolo_tracking.py
@@ -1,4 +1,6 @@
 from ultralytics import YOLO
+import tkinter as tk
+from tkinter import filedialog
 
 # Load an official or custom model
 #model = YOLO("models/yolov8m.pt")
@@ -7,6 +9,16 @@ model = YOLO("models/yolo11n.pt") # Load an official Detect model
 #model = YOLO("models/yolo11n-pose.pt")  # Load an official Pose model
 
 # Perform tracking with the model
+
+# Webcam
 #results = model.track(source=0, show=True)  # Tracking with default tracker
-results = model.track(source=0, show=True, tracker='tracker_config/bytetrack.yaml') # with ByteTrack
-#results = model.track(source=0, show=True, tracker='tracker_config/botsort.yaml') # with BotSort
+#results = model.track(source=0, show=True, tracker='tracker_config/bytetrack.yaml') # with ByteTrack
+#results = model.track(source=0, show=True, tracker='tracker_config/botsort.yaml') # with BoT-SORT
+
+#Video
+# Ask the user for the video file path using a file dialog
+root = tk.Tk()
+root.withdraw()  # Hide the root window
+video_path = filedialog.askopenfilename(title="Select the video file", filetypes=[("Video files", "*.mp4;*.avi;*.mov")])
+
+results = model.track(video_path, show=True)  # Tracking with default tracker
