ee99109631b116aa09dd86a8d8f3e4f33091550a
Adrian Caderno <adriancaderno@gmail.com>
2025-04-06 10:28:03 +0200

    Added comment on training parameters

--- single_gpu_training.py
+++ single_gpu_training.py
@@ -1,5 +1,9 @@
 from ultralytics import YOLO
 
+#Train YOLO11n on the COCO8 dataset for 10 epochs at image size 640. The training device can be specified using the device argument.
+#If no argument is passed GPU device=0 will be used if available, otherwise device='cpu' will be used. 
+
+
 # Load a model
 model = YOLO("yolo11n.yaml")  # build a new model from YAML
 model = YOLO("models/yolo11n.pt")  # load a pretrained model (recommended for training)
